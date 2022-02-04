4e6bfda096ab82bffd3e77787d68f84386026b89
Niklas Donath <niklas.donath@lvdigital.de>
2022-02-04 10:09:27 +0100

    WIP added index view and first draft of form

--- handler.js
+++ handler.js
@@ -1,18 +1,32 @@
-'use strict';
+const serverless = require("serverless-http");
+const hbs = require("hbs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.set("view engine", "hbs");
+app.get("/", function(req, res) {
+  res.status(200).render("index");
+});
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+app.get("/form", function(req, res) {
+  res.status(200).render("form");
+});
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.post("/submit", function(req, res) {
+  let inputIsValid = true
+  // validate input here
+  // ...
+  
+  // input is valid
+  if(inputIsValid) {
+    res.status(200).render("success");
+  } else {
+    // input is invalid
+    res.status(400).render("error");
+  }
+});
+
+module.exports.proptax_collector = serverless(app);
